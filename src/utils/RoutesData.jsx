4720b3015128ca6a9fe39b304b7d10624bbeeaa0
ebayramov <ebayramov06@gmail.com>
2024-10-22 13:46:25 +0400

    changed homepage url

--- src/utils/RoutesData.jsx
+++ src/utils/RoutesData.jsx
@@ -4,7 +4,7 @@ export const routesData = [
     {
         element: <pages.Home/>,
         // element: <pages.TestPage/>,
-        path: "/",
+        path: "/my-portfolio",
         label: "home",
     },
     {
